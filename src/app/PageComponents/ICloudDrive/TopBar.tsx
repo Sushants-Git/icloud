1e69cd9b7555f372892f39d22d1b1a64b3683ae8
Sushants-Git <sushantsgml@gmail.com>
2024-09-19 15:48:22 +0530

    uploading state

--- src/app/PageComponents/ICloudDrive/TopBar.tsx
+++ src/app/PageComponents/ICloudDrive/TopBar.tsx
@@ -21,6 +21,7 @@ import {
 
 const TopBar: React.FC<{ isSidebarOpen: boolean }> = ({ isSidebarOpen }) => {
     const [files, setFiles] = useState<File[]>([]);
+    const [isUploading, setIsUploading] = useState(false);
     const onDrop = useCallback((acceptedFiles: File[]) => {
         setFiles(acceptedFiles);
     }, []);
@@ -30,14 +31,17 @@ const TopBar: React.FC<{ isSidebarOpen: boolean }> = ({ isSidebarOpen }) => {
         onClientUploadComplete: async () => {
             console.log("uploaded successfully!");
             setFiles([]);
+            setIsUploading(false);
             await utils.file.getFiles.invalidate();
         },
         onUploadError: () => {
             console.log("error occurred while uploading");
             setFiles([]);
+            setIsUploading(false);
         },
         onUploadBegin: (fileName) => {
             console.log("upload has begun for", fileName);
+            setIsUploading(true);
         },
     });
 
@@ -51,11 +55,15 @@ const TopBar: React.FC<{ isSidebarOpen: boolean }> = ({ isSidebarOpen }) => {
     useEffect(() => {
         async function upload() {
             if (files.length > 0) {
+                setIsUploading(true);
                 await startUpload(files);
             }
         }
 
-        upload().catch(console.error);
+        upload().catch((error) => {
+            console.error(error);
+            setIsUploading(false);
+        });
     }, [files]);
 
     return (
@@ -72,7 +80,11 @@ const TopBar: React.FC<{ isSidebarOpen: boolean }> = ({ isSidebarOpen }) => {
 
             <div {...getRootProps()}>
                 {!(files.length > 0) && <input {...getInputProps()} />}
-                <Upload className="h-5 w-5 cursor-pointer rounded-md fill-appleBlue p-2 hover:bg-hoverGray" />
+                {isUploading ? (
+                    <div className="h-5 w-5 animate-spin rounded-full border-2 p-2 border-appleBlue border-t-transparent"></div>
+                ) : (
+                    <Upload className="h-5 w-5 cursor-pointer rounded-md fill-appleBlue p-2 hover:bg-hoverGray" />
+                )}
             </div>
 
             <div className="flex items-center space-x-4">
